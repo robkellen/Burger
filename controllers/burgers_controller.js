b04c2f7a8fe64d368e5e5efdb8b8f443db944aef
Rob Kellen <rob.h.kellen@gmail.com>
2020-09-09 15:56:57 -0700

    created the controllers, models, and views directories

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const burger = require("./models/burger.js")
+
+const app = express();
+
+// Create the router for the app, and export the router at the end of your file.
---
+++ models/burger.js
@@ -0,0 +1,7 @@
+const orm = require("./config/orm"); 
+
+// inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
+
+
+
+module.exports = burger;
