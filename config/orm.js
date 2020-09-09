3807f8471a9c4d58400e484c4b7830ea6c3856ba
Rob Kellen <rob.h.kellen@gmail.com>
2020-09-09 15:31:35 -0700

    added skeleton for methods inside orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,22 @@
+//import connection from connection.js
 const connection = require("./connection");
+
+//create methods to execute MySQL commands in controllers
+const orm = {
+  //method to select all burgers from burgers table
+  selectAll: function (table) {
+    const queryString = "SELECT * FROM ?";
+    connection.query(queryString, [table], function (err, res) {
+      if (err) {
+        res.status(500).end();
+      }
+      console.log(res);
+    });
+  },
+  //method to insert a burger into the db
+  insertOne: function () {},
+  //method to update a burger in the db
+  updateOne: function () {},
+};
+
+module.exports = orm;
