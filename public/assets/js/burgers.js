1dc1b0f83e3e551232da87807c198b39091df731
Rob Kellen <rob.h.kellen@gmail.com>
2020-09-11 23:25:10 -0700

    form and button to submit a new burger are working

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,16 +1,32 @@
-$(function(){
-  $(".devourBtn").on("click", function(e){
+$(function () {
+  $(".devourBtn").on("click", function (e) {
     e.preventDefault();
     const id = $(this).data("id");
     const devoured = {
-      devoured: 1
+      devoured: 1,
     };
     $.ajax("/api/burgers/" + id, {
       type: "PUT",
       data: devoured,
-    }).then(function(){
+    }).then(function () {
       console.log("num num num");
       location.reload();
+    });
+  });
+});
+$(function () {
+  $(".addBurgBtn").on("click", function (e) {
+    e.preventDefault();
+    const newBurg = {
+      burger_name: $("#newBurg").val().trim(),
+      devoured: 0,
+    };
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurg
+    }).then(function(){
+      console.log("made a new burger");
+      location.reload();
     })
-  })
-})
+  });
+});
