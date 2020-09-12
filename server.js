80c76650588892fb4bd60024c501ca16576ed254
Rob Kellen <rob.h.kellen@gmail.com>
2020-09-11 21:00:36 -0700

    added handlebars and middleware to server.js.  Set server to PORT 5050.

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 const express = require("express");
-var exphbs = require("express-handlebars");
-var mysql = require("mysql");
 
+const PORT = process.env.PORT || 5050;
 
-
-
-
+const app = express();
 
 // Use the express.static middleware to serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+const routes = require("./controllers/catsController.js");
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
