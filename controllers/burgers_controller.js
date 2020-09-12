c518456e90f2ae48d4d1075d8a13af7d69f7b3ff
Rob Kellen <rob.h.kellen@gmail.com>
2020-09-11 19:27:52 -0700

    added router.post function to burgers_controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,19 +3,27 @@ const express = require("express");
 // Create the router for the app
 const router = express.Router();
 //import the burger.js model to use db functions
-const burger = require("../models/burger")
+const burger = require("../models/burger");
 
-router.get("/", function(req, res){
-  burger.getAll(function(data){
+router.get("/", function (req, res) {
+  burger.getAll(function (data) {
     const hbsObj = {
       burgers: data,
-    }
+    };
     res.render("index", hbsObj);
-  })
-})
-
-
-
+  });
+});
+router.post("/api/burgers", function (req, res) {
+  burger.insert(
+    "burger_name",
+    "devoured",
+    req.body.burger_name,
+    req.body.devoured,
+    function (result) {
+      res.json({ id: result.insertId });
+    }
+  );
+});
 
 // export the router at the end of your file.
 module.exports = router;
